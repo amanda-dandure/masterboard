ce2acaa0be5ffa5d8a83c2d96ac0cc98945d3bc1
Amandah <122104257+amanda-dandure@users.noreply.github.com>
2025-01-05 14:55:25 +0200

    react/typescript refactor version complete

---
+++ react-refactor_version/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Refactored by
+        <a
+          href="https://www.linkedin.com/in/amandah-dandure/"
+          style={{ color: "var(--turquoise)" }}
+        >
+          @amanda_dandure
+        </a>
+      </p>
+    </footer>
+  );
+}
