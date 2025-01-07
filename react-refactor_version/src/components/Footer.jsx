6ff77f09cfa376562fcfbe9de91b29c9c323fff8
Amandah <122104257+amanda-dandure@users.noreply.github.com>
2025-01-07 12:25:12 +0200

    react refactor completed

---
+++ react-refactor_version/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Refactored by
+        <a
+          href="https://github.com/amanda-dandure/masterboard.git"
+          style={{ color: "var(--turquoise)" }}
+        >
+          Masterboard
+        </a>
+      </p>
+    </footer>
+  );
+}
